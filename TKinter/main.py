046fea8efe260c7c85c01aea2e279ad98543cb1e
OniChan-Baka <famalik9876@gmail.com>
2023-04-19 07:18:30 -0700

    4 topic

---
+++ TKinter/main.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+# 1 pack
+# root = Tk()
+# Hello = Label(root, text="Hello, world!")
+# Hello.pack()
+# root.mainloop()
+
+# 2 grid
+# root = Tk()
+# hello = Label(root, text="Hello").grid(row=0, column=0)
+# hello2 = Label(root, text="Hello 2").grid(row=0, column=1)
+# hello3 = Label(root, text="Hello 3").grid(row=0, column=2)
+# root.mainloop()
+
+# 3 button
+# root = Tk()
+# def onClick():
+#     print("Hello")
+# b = Button(root, text="Hello", state="active",
+#            padx=10, pady=10, command=onClick, fg="blue", bg="black")
+# b.pack()
+# root.mainloop()
+
+# 4 inputBox
+# root = Tk()
+# e = Entry(root, width=50, bg="green", fg="white", border=20)
+# e.insert(0, "Enter you name:")  # placeholder
+# def onClick():
+#     print("hello, "+e.get())
+# b = Button(root, text="hello", state="active",
+#            padx=10, pady=10, command=onClick, fg="blue", bg="black")
+# b.pack()
+# e.pack()
+# root.mainloop()
