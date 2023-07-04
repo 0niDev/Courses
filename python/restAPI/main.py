6a030d65098ec8d201e4e7c60799545c3c96ab4c
OniChan-Baka <famalik9876@gmail.com>
2023-07-04 01:38:37 -0700

    server testing

--- python/restAPI/main.py
+++ python/restAPI/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/hello")
 def hello():
-    return "hello: world"
+    return "hello: hello"
 @app.get("/")
 def main():
-    return "hello how are you?"
+    return "hello: main"
